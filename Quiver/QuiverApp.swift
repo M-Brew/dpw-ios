ac8945a5639779808687c8b19fd09058eff924c8
Michael Brew <themichaelbrew@gmail.com>
2025-08-11 14:42:29 +0000

    Initial Commit

---
+++ Quiver/QuiverApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuiverApp.swift
+//  Quiver
+//
+//  Created by Michael Brew on 11/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuiverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuiverTests/QuiverTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuiverTests.swift
+//  QuiverTests
+//
+//  Created by Michael Brew on 11/08/2025.
+//
+
+import Testing
+@testable import Quiver
+
+struct QuiverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
